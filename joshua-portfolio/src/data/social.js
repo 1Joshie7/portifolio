af04f6a520e2b08d7168d56f8ad9ea510b8f8125
Joshie <joshuatevera@gmail.com>
2026-06-05 04:08:13 +0200

    Fix formatting in social.js

--- joshua-portfolio/src/data/social.js
+++ joshua-portfolio/src/data/social.js
@@ -2,6 +2,6 @@ export const socialLinks = {
   github: "https://github.com/1Joshie7",
   email: "joshuatevera@gmail.com",
   phone: "+263 777 056 234",
-  location: "Harare, Zimbabwe"
+  location: "Harare, Zimbabwe",
   linkedin: "https://www.linkedin.com/in/joshua-tevera-3a08a0344",
 };
