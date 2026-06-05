3c2b7652a96de832145bf42b9086fbc8591127f1
Joshie <joshuatevera@gmail.com>
2026-06-05 04:00:38 +0200

    Add LinkedIn profile to social data

--- joshua-portfolio/src/data/social.js
+++ joshua-portfolio/src/data/social.js
@@ -3,4 +3,5 @@ export const socialLinks = {
   email: "joshuatevera@gmail.com",
   phone: "+263 777 056 234",
   location: "Harare, Zimbabwe"
+  linkedin: "https://www.linkedin.com/in/joshua-tevera-3a08a0344",
 };
